977465ad9d73a6c96d0433acf0a9252e4b7baa75
DA-Freddy <Frederic.Fels@develepoerakademie.com>
2025-12-01 13:36:34 +0100

    first commit

---
+++ src/app/add-note-dialog/add-note-dialog.component.ts
@@ -0,0 +1,30 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { NoteListService } from '../firebase-services/note-list.service'
+
+import { FormsModule } from '@angular/forms';
+
+@Component({
+    selector: 'app-add-note-dialog',
+    imports: [FormsModule],
+    templateUrl: './add-note-dialog.component.html',
+    styleUrl: './add-note-dialog.component.scss'
+})
+export class AddNoteDialogComponent {
+  @Output() addDialogClosed: EventEmitter<boolean> = new EventEmitter();
+  title = "";
+  description = "";
+
+  constructor(public noteService: NoteListService){}
+
+  closeDialog() {
+    this.title = "";
+    this.description = "";
+    this.addDialogClosed.emit(false);
+  }
+
+  addNote(){
+    //beachte das closeDialog() zum Schluss kommt, denn es leert die Variablen
+    this.closeDialog();
+  }
+}
