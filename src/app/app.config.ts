e2bc86b53767b1e426fbd6ca474a29cf3539d64b
Jürgen Malinowski <jm-malle@gmx.de>
2025-12-03 11:10:53 +0100

    Connection to Firebase completed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "da-notes-65b19", appId: "1:322182027128:web:f42761facb2ed366cb9a8d", storageBucket: "da-notes-65b19.firebasestorage.app", apiKey: "AIzaSyDfdQ55Oypt4a_2tIxqGPmMblP-cp1vkgs", authDomain: "da-notes-65b19.firebaseapp.com", messagingSenderId: "322182027128", projectNumber: "322182027128", version: "2" })), provideFirestore(() => getFirestore())]
 };
